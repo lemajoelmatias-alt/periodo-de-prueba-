401d57007c69546cedf92a7d5264e0d21d9c0fc3
lemajoelmatias-alt <lemajoelmatias@gmail.com>
2026-04-27 14:06:05 -0300

    Update tp10.js

--- tp10.js
+++ tp10.js
@@ -1,21 +1,16 @@
+function ejercicio10(){
+    let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        let apariciones = new Array(13).fill(0);
-        const totalLanzamientos = 36000;
+    for (var i =0 ; i<36000; i++){
+        let dado1 = matchMedia.floor(Math.ramdom() * 6 ) + 1;
+         let dado2 = matchMedia.floor(Math.ramdom() * 6 ) + 1;
+         let suma = dado1 + dado2
+         arr [suma] = arr[suma] + 1
+        
+    }
+    console.log(arr);
 
-        for (let i = 0; i < totalLanzamientos; i++) {
-            let dado1 = Math.floor(Math.random() * 6) + 1;
-            let dado2 = Math.floor(Math.random() * 6) + 1;
-            let suma = dado1 + dado2;
-            
-            apariciones[suma]++;
-        }
-
-        const tablaBody = document.querySelector("#tablaResultados tbody");
-
-        for (let suma = 2; suma <= 12; suma++) {
-            let fila = `<tr>
-                <td class="highlight">${suma}</td>
-                <td>${apariciones[suma].toLocaleString()}</td>
-            </tr>`;
-            tablaBody.innerHTML += fila;
-        }
+    for(let j = 2; j<13; j++){
+        console.log("el numero "+ j +"salio: "+ arr[j] + "veces en total." );
+    }
+}      
